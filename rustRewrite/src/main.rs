2481bd50498d56a4926c03c28b81369ca83127b2
harley <barley@nobarley.no>
2026-08-18 22:16:24 -0700

    small udpate

--- rustRewrite/src/main.rs
+++ rustRewrite/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[inline(never)]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
 }
